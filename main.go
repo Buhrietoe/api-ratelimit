266b0740b90d7942cb3a7c57d046d20036fb7572
Jason Gardner <buhrietoe@gmail.com>
2018-04-12 00:01:26 -0500

    cli parms

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 )
 
 func main() {
-	err := conf.load("arle.json")
+	confPtr := flag.String("config", "arle.json", "config file")
+	flag.Parse()
+
+	err := conf.load(*confPtr)
 	if err != nil {
 		log.Printf("Error loading config: %s", err)
 	}
