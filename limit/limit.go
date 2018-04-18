cd1965f7a1dd8046d430b67b09ef33b611f42e7b
Jason Gardner <buhrietoe@gmail.com>
2018-04-18 15:44:38 -0500

    documentation

--- limit/limit.go
+++ limit/limit.go
@@ -32,23 +32,29 @@ func (r *Rate) Delay() {
 }
 
 // Check returns true if under limit
-// returns false and delay if over limit
+// Returns false and delay if over limit
+// This implements a FIFO linked list of request times
+// Oldest are at the front, new requests added to back
 func (r *Rate) Check() (ok bool, delay time.Duration) {
 	r.mtx.Lock()
 	defer r.mtx.Unlock()
 
 	now := time.Now()
 
+	// add to request list if not yet at limit
 	if l := r.requests.Len(); l < r.Limit {
 		r.requests.PushBack(now)
 		return true, 0
 	}
 
+	// check if oldest request was sooner than a second
 	front := r.requests.Front()
 	if diff := now.Sub(front.Value.(time.Time)); diff < time.Second {
 		return false, time.Second - diff
 	}
 
+	// oldest request was longer ago than a second
+	// update oldest and rotate to back of list
 	front.Value = now
 	r.requests.MoveToBack(front)
 	return true, 0
