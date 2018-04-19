13e2d5ccdd0c43f8bd923e147746398d16690952
Jason Gardner <buhrietoe@gmail.com>
2018-04-18 23:02:54 -0500

    working at this point

--- api.go
+++ api.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"net/url"
@@ -15,19 +16,47 @@ func storeEvent(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintf(w, "Error: %v", err)
 			return
 		}
-		if len(values.Get("event")) == 0 {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintf(w, "Error: %v", "Wrong input parameter")
+
+		if len(values.Get("event")) > 0 {
+			log.Printf("Got StoreEvent?event")
+
+			// rate limit this request
+			conf.Rate.Delay()
+
+			// rewrite request to pass along to the configured remote
+			r.URL.Scheme = "http"
+			r.Host = conf.Remote.String()
+			r.URL.Host = conf.Remote.String()
+			proxyHTTP(w, r)
 			return
 		}
 
-		// We got something valid
-		conf.Rate.Delay()
-		log.Printf("Got event: %v", values.Get("event"))
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, "ok")
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "Error: %v", "Wrong input parameter")
 	} else {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "%s method not allowed", r.Method)
+	}
+}
+
+func proxyHTTP(w http.ResponseWriter, r *http.Request) {
+	resp, err := http.DefaultTransport.RoundTrip(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusServiceUnavailable)
+		return
+	}
+	defer resp.Body.Close()
+	copyHeader(w.Header(), resp.Header)
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body)
+
+	log.Printf("Proxied Response: %s, Code: %v", r.RequestURI, resp.StatusCode)
+}
+
+func copyHeader(dst, src http.Header) {
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
 	}
 }
